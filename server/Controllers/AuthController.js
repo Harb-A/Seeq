2c88993bf278a6b0b2c98dba978564ab83bfe2da
Sleiman Ghanem <you@example.com>
2023-12-12 05:19:40 +0200

    Update access token expiration time

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -31,7 +31,7 @@ const login = asyncHandler(async (req, res) => {
       },
     },
     process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: "1m" }
+    { expiresIn: "10h" }
   );
 
   const refreshToken = jwt.sign(
